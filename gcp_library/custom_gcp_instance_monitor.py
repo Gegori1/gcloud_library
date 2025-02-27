3d6ebd6e6f02b08af9d672a13554431f9acfd95d
Gegori1 <galvarez.alvarez@iteso.mx>
2025-02-27 10:51:47 -0600

    add custom monitor pause to monitor_cpu_usage and added method get_instace_details to get id and external ip address

--- gcp_library/custom_gcp_instance_monitor.py
+++ gcp_library/custom_gcp_instance_monitor.py
@@ -1,5 +1,8 @@
 from google.cloud import compute_v1
 from google.api_core import exceptions
+from google.cloud import monitoring_v3
+from google.cloud.monitoring_v3.query import Query
+from datetime import datetime, timezone
 import time
 
 class CustomGCPInstanceMonitor:
@@ -44,17 +47,42 @@ class CustomGCPInstanceMonitor:
             print(f"Instance {instance_name} not found. Unable to delete.")
         except Exception as e:
             print(f"Error deleting instance {instance_name}: {e}")
+            
+    def _get_cpu_utilization(self, project_id, instance_name):
+        client = monitoring_v3.MetricServiceClient()
+        project_name = project_id
 
-    def monitor_cpu_usage(self, instance_name, threshold=10.0, check_interval=60):
+        # Define the time interval for the data you want to fetch
+        now = datetime.now(tz=timezone.utc)
+
+        # Create a query for the CPU utilization metric
+        query = Query(
+            client,
+            project_name,
+            "compute.googleapis.com/instance/cpu/utilization",
+            end_time=now,
+            minutes=5
+        )
+
+        # Filter by instance and zone
+        query = query.select_metrics(instance_name=instance_name)
+
+        # Execute the query and fetch the data
+        try:
+            results = list(query)
+            return results[0].points[0].value.double_value * 100
+        except:
+            return None
+
+    def monitor_cpu_usage(self, instance_name, threshold=10.0, check_interval=60, startup_pause_time=240):
         """Monitors CPU usage and stops the instance if usage is below threshold."""
         print(f"Waiting for the metric to start on instance {instance_name}")
-        time.sleep(60 * 4)  # Wait for metrics to start
+        time.sleep(startup_pause_time)  # Wait for metrics to start
         retry = 0
         while True:
             # max_retries times if CPU usage data is not immediately available.
             try:
-                # Use gcloud command to get CPU utilization
-                # cpu_usage = get_cpu_utilization(self.project_id, instance_name) # I need to install google-cloud-monitoring
+                cpu_usage = self._get_cpu_utilization(self.project_id, instance_name) # I need to install google-cloud-monitoring
                 cpu_usage = 5 # temporal value
                 if cpu_usage:
                     print(f"CPU Usage for {instance_name}: {cpu_usage:.2f}%")
@@ -67,7 +95,7 @@ class CustomGCPInstanceMonitor:
                     if retry < 3:
                         retry += 1
                         print(f"Could not retrieve CPU usage for {instance_name}. retrying...")
-                        time.sleep(60)
+                        time.sleep(check_interval)
                         continue
                     else:
                         print(f"Could not retrieve CPU for {instance_name} for the second time. Stopping metric.")
@@ -78,3 +106,23 @@ class CustomGCPInstanceMonitor:
             except Exception as e:
                 print(f"Error monitoring CPU usage for {instance_name}: {e}")
                 break
+
+    def get_instance_details(self, instance_name):
+        """Retrieves the instance ID and external IP address of a GCP instance."""
+        try:
+            instance = self.compute_client.get(project=self.project_id, zone=self.zone, instance=instance_name)
+            instance_id = instance.id
+            external_ip = instance.network_interfaces[0].access_configs[0].nat_i_p
+            return instance_id, external_ip
+        except exceptions.NotFound:
+            print(f"Instance {instance_name} not found.")
+            return None, None
+        except Exception as e:
+            print(f"Error getting instance details for {instance_name}: {e}")
+            return None, None
+
+# %%
+
+
+# %%
+
